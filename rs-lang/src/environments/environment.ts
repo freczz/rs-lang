244eac9086fd9fade221b8dd89c412be80ee7c2d
Dmitry-NBook <pdo.hardwork@yandex.ru>
2022-02-03 16:47:21 +0300

    feat: add airbnb style guide

--- rs-lang/src/environments/environment.ts
+++ rs-lang/src/environments/environment.ts
@@ -2,8 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+const environment = {
+  production: false,
 };
 
 /*
@@ -14,3 +14,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+export default environment;
