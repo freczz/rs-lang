5aec9dd1b7c4b2c315e588f29d008fe87e19a2f8
Alexey <alexmikhaylovich@mail.ru>
2022-02-08 02:19:24 +0300

    feat: add main page

--- rs-lang/src/app/app.module.ts
+++ rs-lang/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: AppRoutingModule =[
+    { path: '', component: MainPageComponent},
+];
 
 import { RSLState } from './store/rsl.state';
 import { NgxsModule } from '@ngxs/store';
@@ -10,12 +17,17 @@ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import environment from 'src/environments/environment';
 
 @NgModule({
+<<<<<<< HEAD
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule,
     NgxsModule.forRoot([RSLState], {
       developmentMode: !environment.production,
     }),
     NgxsReduxDevtoolsPluginModule.forRoot(),],
+=======
+  declarations: [AppComponent, MainPageComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
+>>>>>>> fbcd45a (feat: add main page)
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ rs-lang/src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  isActive: boolean = false;
+
+  toggleMenu(): void {
+    this.isActive = !this.isActive;
+  }
+
+}
