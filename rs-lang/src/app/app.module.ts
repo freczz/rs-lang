8fc477b71d1359d5a4e6e5ab579762faaf033027
Dmitry-NBook <pdo.hardwork@yandex.ru>
2022-02-09 00:28:09 +0300

    feat: create rsl-state

--- rs-lang/src/app/app.module.ts
+++ rs-lang/src/app/app.module.ts
@@ -4,9 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 
+import { RSLState } from './store/rsl.state';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import environment from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule,
+    NgxsModule.forRoot([RSLState], {
+      developmentMode: !environment.production,
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),],
   providers: [],
   bootstrap: [AppComponent],
 })
