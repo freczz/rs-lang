b05ae541c799a56d5209ca16d4643181940a4093
VladKocherga <vlad.kocherga228@gmail.com>
2022-02-13 02:54:38 +0300

    feat: create basic component audiocall-game

--- rs-lang/src/app/app.module.ts
+++ rs-lang/src/app/app.module.ts
@@ -4,38 +4,28 @@ import environment from 'src/environments/environment';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
-import RSLState from './store/rsl.state';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import MainPageComponent from './components/main-page/main-page.component';
 import HeaderComponent from './components/header/header.component';
 import FooterComponent from './components/footer/footer.component';
-import SprintGameComponent from './components/sprint-game/sprint-game.component';
-import ButtonComponent from './components/sprint-game/button/button.component';
-import RoundComponent from './components/sprint-game/round/round.component';
-import StartComponent from './components/sprint-game/start/start.component';
-import LoadingComponent from './components/sprint-game/loading/loading.component';
-import ResultSprintComponent from './components/sprint-game/result-sprint/result-sprint.component';
+
+import RSLState from './store/rsl.state';
+import { AudiocallGameComponent } from './components/audiocall-game/audiocall-game.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent,
-    HeaderComponent,
-    FooterComponent,
-    SprintGameComponent,
-    ButtonComponent,
-    RoundComponent,
-    StartComponent,
-    LoadingComponent,
-    ResultSprintComponent,
-  ],
+  declarations: [AppComponent, MainPageComponent, HeaderComponent, FooterComponent, AudiocallGameComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
     NgxsModule.forRoot([RSLState], {
       developmentMode: !environment.production,
     }),
@@ -44,4 +34,4 @@ import ResultSprintComponent from './components/sprint-game/result-sprint/result
   providers: [],
   bootstrap: [AppComponent],
 })
-export default class AppModule {}
+export default class AppModule { }
---
+++ rs-lang/src/app/components/audiocall-game/audiocall-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audiocall-game',
+  templateUrl: './audiocall-game.component.html',
+  styleUrls: ['./audiocall-game.component.scss']
+})
+export class AudiocallGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
